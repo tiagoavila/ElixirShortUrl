75e06420252d209a082c71660523e55fcc25ca79
Tiago Avila <tiago.avila09@gmail.com>
2023-11-02 16:24:59 -0300

    release updates

--- lib/url_shorter/url_shorter_bloom_filter.ex
+++ lib/url_shorter/url_shorter_bloom_filter.ex
@@ -4,39 +4,40 @@ defmodule UrlShorter.UrlShorterBloomFilter do
   alias UrlShorter.UrlShortenerContext
 
   # Server Callbacks
-
-  def start_link(bloom_filter \\ []) do
-    IO.puts ("**** BLOOM FILTER GENSERVER STARTED... ****")
-
-    GenServer.start_link(__MODULE__, bloom_filter, name: __MODULE__)
-  end
-
+  
   @doc """
   GenServer.init/1 callback
   """
   def init(_state) do
     bloom_filter = UrlShortenerContext.list_urls()
     |> Enum.reduce(Bloomex.scalable(1000, 0.1, 0.1, 2, &Murmur.hash_x86_128/1), fn url, bf -> Bloomex.add(bf, url) end)
-
+    
     IO.puts("**** BLOOM FILTER INITIALIZED ****")
-
+    
     {:ok, bloom_filter}
   end
-
+  
   def handle_call({:contains_url, url}, _from, bloom_filter) do
     {:reply, Bloomex.member?(bloom_filter, url), bloom_filter}
   end
-
+  
   def handle_cast({:add_url, url}, bloom_filter) do
     IO.puts("#{url} ADDED TO BLOOM FILTER")
 
     {:noreply, Bloomex.add(bloom_filter, url)}
   end
-
+  
   # ===================================================================================================
-
+    
   # Client Interface
+    
+  @spec start_link(any) :: :ignore | {:error, any} | {:ok, pid}
+  def start_link(bloom_filter \\ []) do
+    IO.puts ("**** BLOOM FILTER GENSERVER STARTED... ****")
 
+    GenServer.start_link(__MODULE__, bloom_filter, name: __MODULE__)
+  end
+    
   def contains_url?(url) do
     GenServer.call(__MODULE__, {:contains_url, url})
   end
